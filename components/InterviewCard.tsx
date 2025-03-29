f996e51e9479653a090d4e96f33421285bfe828f
sebastien lato <sebastienlato@gmail.com>
2025-03-29 10:33:06 -0400

    implement interview session

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,7 +5,7 @@ import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "@/components/DisplayTechIcons";
 
-const InterviewCard = async ({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+const InterviewCard = async ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
@@ -46,8 +46,8 @@ const InterviewCard = async ({ interviewId, userId, role, type, techstack, creat
 
                     <Button className="btn-primary">
                         <Link href={feedback
-                        ? `/interview/${interviewId}/feedback`
-                        : `/interview/${interviewId}`}>
+                        ? `/interview/${id}/feedback`
+                        : `/interview/${id}`}>
                             {feedback ? 'Check Feedback' : 'View Interview'}
                         </Link>
                     </Button>
